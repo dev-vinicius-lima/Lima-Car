8904eee52233189fb1baf8bfc38da12d2e232eb1
Vinicius Lima <162653125+vinicius-lima-elystech@users.noreply.github.com>
2024-03-19 17:09:41 -0400

    feat: i started project ecomerce the car

---
+++ src/pages/dashboard/new/index.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const New = () => {
+  return <div>New CAR</div>;
+};
+
+export default New;
---
+++ src/pages/register/index.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Register = () => {
+  return <div>pagina Register</div>;
+};
+
+export default Register;
