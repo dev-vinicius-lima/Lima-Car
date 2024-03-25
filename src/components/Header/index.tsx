a1e651e27eed14c32e5b0707b8e8b3aa786e38b4
Vinicius Lima <162653125+vinicius-lima-elystech@users.noreply.github.com>
2024-03-25 17:27:25 -0400

    .

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -3,10 +3,15 @@ import { AuthContext } from "../../context/AuthContext";
 import logoLima from "../../assets/lima-veiculos.jpg";
 import { Link } from "react-router-dom";
 import { FiUser, FiLogIn } from "react-icons/fi";
+import { signOut } from "firebase/auth";
+import { auth } from "../../services/firebaseConnect";
 
 const Header = () => {
   const { signed, loadingAuth } = useContext(AuthContext);
 
+  async function handleLogout() {
+    await signOut(auth);
+  }
   return (
     <div className="w-full flex items-center justify-center h-18 bg-white drop-shadow mb-4">
       <header className="flex w-full max-w-7xl items-center justify-between px-10 mx-auto">
@@ -14,9 +19,13 @@ const Header = () => {
           <img src={logoLima} alt="logo do site" className="w-full h-20" />
         </Link>
         {!loadingAuth && signed && (
-          <Link to={"/dashboard"}>
+          <Link to={"/dashboard"} className="flex gap-3">
             <div className="border-2 rounded-full p-1 border-gray-900">
-              <FiLogIn size={24} color="#000" />
+              <FiUser size={24} color="#000" />
+            </div>
+
+            <div className="border-2 rounded-full p-1 border-gray-900">
+              <FiLogIn size={24} color="#000" onClick={handleLogout} />
             </div>
           </Link>
         )}
